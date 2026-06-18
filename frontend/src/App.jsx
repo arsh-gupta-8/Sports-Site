faf84d0edcb48fdeb06dbfe6fe5c44cc8cd3fa37
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-18 17:59:20 +0700

    Implemented and formatted data for world cup API call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,33 +4,60 @@ import axios from "axios";
 function app() {
 
     const [matchData, setMatchData] = useState([]);
+    const [webError, setWebError] = useState("");
 
     useEffect(() => {
         const fetchMatches = async () => {
-            const response = await fetch('http://localhost:5000/api/matches');
+            const response = await fetch('https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json');
+
+            const matchInfo = await response.json()
+            console.log(matchInfo)
+            const data = matchInfo.matches
+
+            let i = 1;
+            data.forEach(match => {
+                match.id = i;
+                i++;
+            })
 
             if (response.ok) {
-                setMatchData(await response.json());
+                setMatchData(data);
+                setWebError("");
+            }
+            else {
+                setWebError("Unable to retrieve match data. Please try again later.");
             }
         };
 
         fetchMatches();
+        console.log(matchData)
     }, []);
 
     return (
         <div>
-            <h1>Matchs Today</h1>
-            { matchData.length === 0 ? (
-                <p>No matches today</p>
+            <h1>Matches Today</h1>
+            { webError !== "" ? (
+                <p>{webError}1</p>
             ) : (
-                matchData.map((match) => (
-                    <div>
-                        <h2>Match {match.id}</h2>
-                        <p><strong>{match.team1}</strong> vs <strong>{match.team2}</strong></p>
-                        <p>SCORE     {match.score}</p>
-                    </div>
-                ))
-            )};
+                matchData.length === 0 ? (
+                    <p>No matches today</p>
+                ) : (
+                    matchData.map((match) => (
+                        <div key={match.id}>
+                            <h2>{match.round}</h2>  
+                            <p><strong>{match.team1}</strong> vs <strong>{match.team2}</strong></p>
+                            { Object.hasOwn(match, "score") == true ? (
+                                <div>
+                                    <p>FINAL</p>
+                                    <p>{match.score.ft[0]} - {match.score.ft[1]}</p>
+                                </div>
+                            ) : (
+                                <p>TBD</p>
+                            )}
+                        </div>
+                    ))
+                )
+            )}
         </div>
     );
 }
