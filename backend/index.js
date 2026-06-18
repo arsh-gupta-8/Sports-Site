a44d37c49dc66f49036b8527c121472bc2dfc356
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-18 12:50:03 +0700

    Connected react web page to node server using API calls

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,34 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-let data = ['Arsh']
-
-// WEBSITE ENDPOINTS
-
-app.get('/', (req, res) => {
-    console.log('Reached an endpoint', req.method)
-    res.send(`
-        <body 
-        style="background:yellow;
-        color: green;">
-            <h1>DATA</h1>
-            <p>${JSON.stringify(data)}</p>
-        </body>`)
-})
-
-app.get('/dashboard', (req, res) => {
-    console.log('I hit the dashboard endpoint')
-    res.send('<h1>Dashboard</h1>')
-})
+let data = [{id: 1, team1: 'Argentina', team2: 'Algeria', score: '3-0'}];
 
 // API ENDPOINTS
 
-app.get('/api/data', (req, res) => {
-    console.log('Data requested')
-    res.send(data)
-})
+app.get('/api/matches', (req, res) => {
+    console.log('Data requested');
+    res.send(data);
+});
 
 app.post('/api/data', (req, res) => {
-    const dataEntry = req.body
-    data.push(dataEntry.name)
-    console.log(dataEntry.name)
-    res.sendStatus(201)
-})
+    const dataEntry = req.body;
+    data.push(dataEntry.name);
+    console.log(dataEntry.name);
+    res.sendStatus(201);
+});
 
 app.delete('/api/data', (req, res) => {
-    data.pop()
-    console.log("Deleted an element from the data list")
-    res.sendStatus(204)
-})
+    data.pop();
+    console.log("Deleted an element from the data list");
+    res.sendStatus(204);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server has started on: ${PORT}`)
-})
+    console.log(`Server has started on: ${PORT}`);
+});
