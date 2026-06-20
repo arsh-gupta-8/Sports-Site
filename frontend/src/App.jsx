9f7cf88739ef8d85bbdd4f9dff2f17fb8accd8b5
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-20 16:08:19 +0700

    Added organisation by date to data display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,44 @@ function app() {
 
     useEffect(() => {
         const fetchMatches = async () => {
-            const response = await fetch('https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json');
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/openfootball/worldcup.json/master/2026/worldcup.json');
 
-            const matchInfo = await response.json()
-            console.log(matchInfo)
-            const data = matchInfo.matches
+                const matchInfo = await response.json()
+                console.log(matchInfo)
+                const data = matchInfo.matches
 
-            let i = 1;
-            data.forEach(match => {
-                match.id = i;
-                i++;
-            })
+                let i = 1;
+                data.forEach(match => {
+                    match.id = i;
+                    i++;
+                })
 
-            if (response.ok) {
-                setMatchData(data);
-                setWebError("");
-            }
-            else {
+                if (response.ok) {
+                    let matchByDate = []
+                    if (data.length !== 0) {
+                        let searchDate = data[0].date;
+                        let matchSet = [];
+                        for (i = 0; i < data.length; i++) {
+                            if (data[i].date === searchDate) {
+                                matchSet.push(data[i]);
+                            }
+                            else {
+                                searchDate = data[i+1].date;
+                                matchByDate.push(matchSet);
+                                matchSet = [];
+                            }
+                        }
+                        matchByDate.push(matchSet);
+                    }
+                    setMatchData(matchByDate);
+                    setWebError("");
+                }
+                else {
+                    setWebError("Unable to retrieve match data. Please try again later.");
+                }
+            } catch (error) {
+                console.log(error)
                 setWebError("Unable to retrieve match data. Please try again later.");
             }
         };
@@ -35,31 +56,39 @@ function app() {
 
     return (
         <div>
-            <h1>Matches Today</h1>
+            <h1>Matches</h1>
             { webError !== "" ? (
                 <p>{webError}1</p>
+            ) : matchData.length === 0 ? (
+                <p>No matches available</p>
             ) : (
-                matchData.length === 0 ? (
-                    <p>No matches today</p>
-                ) : (
-                    matchData.map((match) => (
-                        <div key={match.id}>
-                            <h2>{match.round}</h2>  
-                            <p><strong>{match.team1}</strong> vs <strong>{match.team2}</strong></p>
-                            { Object.hasOwn(match, "score") == true ? (
-                                <div>
-                                    <p>FINAL</p>
-                                    <p>{match.score.ft[0]} - {match.score.ft[1]}</p>
+                matchData.map((matchSet) => (
+                    <div>
+                        <h2>{matchSet[0].date}</h2>
+                        <h2>{matchSet[0].round}</h2>
+                        <div>
+                            
+                            {matchSet.map((match) => (
+                                <div key={match.id}>
+                                    <h2>{match.id}</h2>  
+                                    <p><strong>{match.team1}</strong> vs <strong>{match.team2}</strong></p>
+                                    { Object.hasOwn(match, "score") == true ? (
+                                        <div>
+                                            <p>FINAL</p>
+                                            <p>{match.score.ft[0]} - {match.score.ft[1]}</p>
+                                        </div>
+                                    ) : (
+                                        <p>TBD</p>
+                                    )}
                                 </div>
-                            ) : (
-                                <p>TBD</p>
-                            )}
+                            ))}
                         </div>
-                    ))
-                )
+                    </div>
+                ))
             )}
         </div>
     );
 }
 
 export default app;
+
