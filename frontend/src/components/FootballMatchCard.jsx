b0de86b0d1c67f60729a9b8f60362afbf818625b
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-23 13:50:37 +0700

    Added country flag object

--- frontend/src/components/FootballMatchCard.jsx
+++ frontend/src/components/FootballMatchCard.jsx
@@ -1,3 +1,55 @@
+
+const worldCupCountryFlags = {
+    "Algeria": "https://flagcdn.com/w320/dz.png",
+    "Argentina": "https://flagcdn.com/w320/ar.png",
+    "Australia": "https://flagcdn.com/w320/au.png",
+    "Austria": "https://flagcdn.com/w320/at.png",
+    "Belgium": "https://flagcdn.com/w320/be.png",
+    "Bosnia & Herzegovina": "https://flagcdn.com/w320/ba.png",
+    "Brazil": "https://flagcdn.com/w320/br.png",
+    "Canada": "https://flagcdn.com/w320/ca.png",
+    "Cape Verde": "https://flagcdn.com/w320/cv.png",
+    "Colombia": "https://flagcdn.com/w320/co.png",
+    "DR Congo": "https://flagcdn.com/w320/cd.png",
+    "Ivory Coast": "https://flagcdn.com/w320/ci.png",
+    "Croatia": "https://flagcdn.com/w320/hr.png",
+    "Curaçao": "https://flagcdn.com/w320/cw.png",
+    "Czech Republic": "https://flagcdn.com/w320/cz.png",
+    "Ecuador": "https://flagcdn.com/w320/ec.png",
+    "Egypt": "https://flagcdn.com/w320/eg.png",
+    "England": "https://flagcdn.com/w320/gb-eng.png",
+    "France": "https://flagcdn.com/w320/fr.png",
+    "Germany": "https://flagcdn.com/w320/de.png",
+    "Ghana": "https://flagcdn.com/w320/gh.png",
+    "Haiti": "https://flagcdn.com/w320/ht.png",
+    "Iran": "https://flagcdn.com/w320/ir.png",
+    "Iraq": "https://flagcdn.com/w320/iq.png",
+    "Japan": "https://flagcdn.com/w320/jp.png",
+    "Jordan": "https://flagcdn.com/w320/jo.png",
+    "Mexico": "https://flagcdn.com/w320/mx.png",
+    "Morocco": "https://flagcdn.com/w320/ma.png",
+    "Netherlands": "https://flagcdn.com/w320/nl.png",
+    "New Zealand": "https://flagcdn.com/w320/nz.png",
+    "Norway": "https://flagcdn.com/w320/no.png",
+    "Panama": "https://flagcdn.com/w320/pa.png",
+    "Paraguay": "https://flagcdn.com/w320/py.png",
+    "Portugal": "https://flagcdn.com/w320/pt.png",
+    "Qatar": "https://flagcdn.com/w320/qa.png",
+    "Saudi Arabia": "https://flagcdn.com/w320/sa.png",
+    "Scotland": "https://flagcdn.com/w320/gb-sct.png",
+    "Senegal": "https://flagcdn.com/w320/sn.png",
+    "South Africa": "https://flagcdn.com/w320/za.png",
+    "South Korea": "https://flagcdn.com/w320/kr.png",
+    "Spain": "https://flagcdn.com/w320/es.png",
+    "Sweden": "https://flagcdn.com/w320/se.png",
+    "Switzerland": "https://flagcdn.com/w320/ch.png",
+    "Tunisia": "https://flagcdn.com/w320/tn.png",
+    "Turkey": "https://flagcdn.com/w320/tr.png",
+    "USA": "https://flagcdn.com/w320/us.png",
+    "Uruguay": "https://flagcdn.com/w320/uy.png",
+    "Uzbekistan": "https://flagcdn.com/w320/uz.png"
+};
+
 function FootballCard({matchInfo}) {
     return (
         <div className="match-card" style={{
@@ -17,6 +69,8 @@ function FootballCard({matchInfo}) {
             </div>
             <div className="match-teams">
                 <p>{matchInfo.team1} vs {matchInfo.team2}</p>
+                <img src={worldCupCountryFlags[matchInfo.team1]} alt={matchInfo.team1}></img>
+                <img src={worldCupCountryFlags[matchInfo.team2]} alt={matchInfo.team2}></img>
             </div>
             <div className="match-score">
                 { Object.hasOwn(matchInfo, "score") == true ? (
