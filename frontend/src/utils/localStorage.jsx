aa219695e58b17993c39e2a42264b9e01d186a21
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-26 23:46:53 +0700

    Added local storage access to handle website currency data and user info

---
+++ frontend/src/utils/localStorage.jsx
@@ -0,0 +1,16 @@
+export function setItem(key, value) {
+    try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export function getItem(key, value) {
+    try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : undefined;
+    } catch (err) {
+        console.log(err);
+    }
+}
