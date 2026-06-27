29ec77679810423ba7e2c165324bbd9e259c8b5d
arsh-gupta-8 <arshmailing@gmail.com>
2026-06-27 12:09:07 +0700

    Added css formatting to navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 import WorldCup from "./pages/WorldCup"
 import Home from "./pages/Home"
+import Navbar from "./components/Navbar";
 
 function App() {
-
+    
     return (
-        <main className="main-content">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/WorldCup" element={<WorldCup />} />
-            </Routes>
-        </main>
+
+        <div>
+            <Navbar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/WorldCup" element={<WorldCup />} />
+                </Routes>
+            </main>
+        </div>
     );
 
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 function Navbar() {
-    const [currency, setCurrency] = useState(() => {
-        const item = getItem("Currency");
-        return item || 1000;
-    });
-
     return (
-        <nav className="navbar">
+        <nav className="navbar" style={{
+            color: "blue",
+            display: "flex",
+            justifyContent: "space-around",
+            backgroundColor: "#464646"
+        }}>
             <div className="Logo">
-
+                <p style={{ textDecoration: "none", color: "#49a6f1", padding: "10px", fontWeight: "bold"}}>Sporta</p>
             </div>
-            <div className="Tabs">
-
+            <div className="Tabs" style={{
+                display: "flex",
+                gap: "30px"
+            }}>
+                <Link to="/" style={{ textDecoration: "none", color: "#9bc3f1", padding: "10px" }}>Home</Link>
+                <Link to="/WorldCup" style={{ textDecoration: "none", color: "#9bc3f1", padding: "10px" }}>World Cup</Link>
             </div>
         </nav>
     );
